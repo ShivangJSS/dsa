7e264dbbb75dc2096c46dc134b7b0863a5f29865
shivang singh <shivangsinghblw@gmail.com>
2026-01-20 02:41:16 +0530

    bubble sort

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();        // size of array
+        int arr[] = new int[n];
+
+        // 🔹 take array input
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 🔹 bubble sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // 🔹 print sorted array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
