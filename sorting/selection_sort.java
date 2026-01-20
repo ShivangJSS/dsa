ffbe97750920435a464b437153b4f31c67c2d991
shivang singh <shivangsinghblw@gmail.com>
2026-01-21 03:01:01 +0530

    next commit

--- sorting/selection_sort.java
+++ sorting/selection_sort.java
@@ -1,3 +1,36 @@
+
+import java.util.*;
 public class selection_sort {
-    
+    public static  void main(String []args)
+    {
+    int n;
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=sc.nextInt();
+    }
+
+     
+    for(int i=0;i<n-1;i++)
+    {
+        int min_idx=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[min_idx])
+            {
+                min_idx=j;
+            }
+        }
+        int temp=arr[min_idx];
+        arr[min_idx]=arr[i];
+        arr[i]=temp;
+    }
+    for(int i=0;i<n;i++)
+    {
+        System.out.print(arr[i]+" ");
+    }
 }
+}
+    
